8d8865146af3ae3a79d120b783ddb9761eb309f5
IsaCristine <bell2011jf@gmail.com>
2022-05-28 16:59:24 -0300

    Inicia criação de rotas

---
+++ monstershop/app/Controllers/ViewController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\App;
+use Exception;
+
+class ViewController
+{
+
+    public function quemSomos()
+    {
+        return view('quem-somos');
+    }
+
+    public function create()
+    {
+ 
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function edit()
+    {
+  
+    }
+
+    public function update()
+    {
+        
+    }
+
+    public function delete()
+    {
+ 
+    }
+}
--- monstershop/app/routes.php
+++ monstershop/app/routes.php
@@ -3,5 +3,6 @@
 use App\Controllers\ExampleController;
 use App\Core\Router;
 
+$router->get('quem-somos', 'viewController@quemSomos')
 
 ?> 

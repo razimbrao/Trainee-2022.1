4e45230b279a9724a877fb6e3fdbac1d05b47bcf
Fredericodgv <fredericodgv@gmail.com>
2022-05-30 19:01:18 -0300

    Arrumando CSS de quem somos

--- monstershop/app/views/site/quem-somos.view.php
+++ monstershop/app/views/site/quem-somos.view.php
@@ -7,6 +7,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="../../../public/css/quem-somos.css">
     <link rel="stylesheet" href="../../../public/css/navbarft.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/2b94009ee2.js" crossorigin="anonymous"></script>
 
     <title>Quem Somos?</title>
 </head>
@@ -15,6 +19,7 @@
 <body>
         <?php require 'navbar.php' ?>
 
+        <div class="body-quemSomos">
         <div class="quem_somos">
             <h1>Quem Somos?</h1>
 
@@ -66,6 +71,7 @@
                     <img src="../../../public/img/aperto-de-maos.gif" alt="aperto-de-maos"></a>
             </div>
             </div>
+        </div>
         </div>
 
         <?php require 'footer.php' ?>

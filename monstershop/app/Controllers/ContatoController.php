a15f50b959e06d474e938d1cee586ba6b489db97
Fredericodgv <fredericodgv@gmail.com>
2022-06-04 23:10:43 -0300

    Adiciona arquivo do controller

---
+++ monstershop/app/Controllers/ContatoController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\App;
+use Exception;
+
+class ContatoController
+{
+
+    public function view()
+    {
+        return view('site/contato');
+    }
+
+    public function show()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function store()
+    {
+    }
+
+    public function edit()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function delete()
+    {
+    }
+}

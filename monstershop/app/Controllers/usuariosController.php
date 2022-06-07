b4d9d7e6452487a47184b45bcaf6ecebebd61fab
Fredericodgv <fredericodgv@gmail.com>
2022-06-06 23:14:00 -0300

    merge com usuarios para atualizar rotas e controller

---
+++ monstershop/app/Controllers/usuariosController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\App;
+use Exception;
+
+class UsuariosController
+{
+
+    public function view()
+    {
+        $usuarios = App::get('database')->selectAll('usuarios');
+
+        $tabela = [
+            'usuarios' => $usuarios
+        ];        
+
+        return view('/admin/usuarios', $tabela);
+    }
+
+    public function create()
+    {
+        $dados = [
+            'nome' => $_POST['nome'],
+            'email' => $_POST['email'],
+            'senha' => $_POST['senha'],
+            'foto' => $_POST['foto']
+        ];
+
+        App::get('database')->adicionaUsuario('usuarios', $dados);
+
+        header('Location: /admin/usuarios');
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function edit()
+    {
+  
+    }
+
+    public function update()
+    {
+        $dados = [
+            'nome' => $_POST['nome'],
+            'email' => $_POST['email'],
+            'senha' => $_POST['senha'],
+        ];
+
+        $foto = $_POST['foto'];
+
+        App::get('database')->editaUsuario('usuarios', $dados, $foto, $_POST['id']);
+
+        header('Location: /admin/usuarios');
+    }
+
+    public function delete()
+    {
+        $id = $_POST['id'];
+
+        App::get('database')->deletaUsuario('usuarios', $id);
+
+        header('Location: /admin/usuarios');
+    }
+}

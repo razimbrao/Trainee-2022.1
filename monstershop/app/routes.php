84e5f6f61b6eff4d5577e7408b96e6d162477124
Fredericodgv <fredericodgv@gmail.com>
2022-06-06 22:46:46 -0300

    Atualiza rotas

--- monstershop/app/routes.php
+++ monstershop/app/routes.php
@@ -12,8 +12,32 @@ $router->get('home', 'ViewController@home');
 
 $router->get('produtos', 'ViewController@produtos');
 
+
 //-----------Rota de Contato-------------//
 
 $router->get('contato', 'ContatoController@view');
+
+
+//---------Rotas de Usuários----------//
+
+$router->get('admin/usuarios', 'UsuariosController@view');
+
+$router->post('admin/usuarios/adicionar', 'UsuariosController@create');
+
+$router->post('admin/usuarios/excluir', 'UsuariosController@delete');
+
+$router->post('admin/usuarios/editar', 'UsuariosController@update');
+
+
+//---------Rotas de Categorias----------//
+
+$router->get('admin/categorias', 'CategoriasController@index');
+$router->post('admin/categorias', 'CategoriasController@index');
+
+$router->post('admin/categorias/create', 'CategoriasController@create');
+
+$router->post('admin/categorias/delete', 'CategoriasController@delete');
+
+$router->post('admin/categorias/edit', 'CategoriasController@update');
 
 ?> 

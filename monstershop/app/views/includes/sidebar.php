86e849c2b7966e83ea9dec6d932be608fc5abce1
fredericodgv <fredericodgv@gmail.com>
2022-06-07 12:37:56 -0300

    renomeia sdebar para php

---
+++ monstershop/app/views/includes/sidebar.php
@@ -0,0 +1,36 @@
+<input type="checkbox" id="check">
+
+<div class="retangle">
+
+    <div class="btn-menu">
+        <label class="label1" for="check">
+            <ion-icon class="icone1" name="menu-outline" id="sidebar_btn"></ion-icon>
+        </label>
+    </div>
+
+    <img class="imgname" src="../../../public/img/MonsterShop-name.png " alt="MonsterShop">
+
+</div>
+
+<!--sidebar começo-->
+<div class="sidebar">
+    <center>
+        <img src="../../../public/img/monsterShop_oficial.png" class="image" alt="">
+        <h2>Admin</h2>
+    </center>
+    <a href="#"><i class="fa-solid fa-user-gear"></i><span> Dashboard</span></a>
+    <a href="#"><i class="fa-solid fa-users"> </i><span> Usuários</span></a>
+    <a href="#"><i class="fa-solid fa-cart-shopping"> </i><span> Produtos</span></a>
+    <a href="#"><i class="fa-solid fa-list"> </i><span> Categorias</span></a>
+    <a href="#"><i class="fa-solid fa-arrow-right-from-bracket"></i> </i><span> Sair</span></a>
+
+</div>
+<!--sidebar final-->
+<div class="content"></div>
+
+
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous"></script>

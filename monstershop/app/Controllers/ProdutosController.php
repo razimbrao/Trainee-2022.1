bc407361d056a118f4b8af3c2c45620c72d2e6ed
GabrielBahia <gabriel.bahia@estudante.ufjf.br>
2022-06-08 10:18:04 -0300

    Implementando paginacao na pagina de produtos (falta arrumar a paginacao quando tem filtro)

--- monstershop/app/Controllers/ProdutosController.php
+++ monstershop/app/Controllers/ProdutosController.php
@@ -8,17 +8,69 @@ use Exception;
 class ProdutosController
 {
     public function index()
-    {
+    {   
+        $page = 1;
+
         if(!empty($_POST['categoria'])) {
+
+            if (isset($_GET['pagina']) && !empty($_GET['pagina']))
+            {
+                $page = intval($_GET['pagina']);
+
+                if ($page <= 0)
+                {
+                    return redirect('site/produtos');
+                }
+            }
+
+            $items_per_page = 9;
+            $start_limit = $items_per_page * $page - $items_per_page;
+
             $categoria = filter_input(INPUT_POST, 'categoria', FILTER_SANITIZE_SPECIAL_CHARS);  
             $produtos = App::get('database')->procurar('produtos', 'categoria', $categoria);
             $categorias = App::get('database')->selectAll('categorias');
-            return view('site/produtos', compact('produtos', 'categorias'));  
+
+            $rows_count = App::get('database')->countAll('produtos');
+            echo($rows_count);
+            echo(" ");
+
+            echo($items_per_page);
+            
+            
+            if ($start_limit > $rows_count) {
+                return redirect('site/produtos');
+            }
+
+            $total_pages = ceil($rows_count / $items_per_page);
+            echo($total_pages);
+            exit();
+            
+            return view('site/produtos', compact('produtos', 'categorias', 'page', 'total_pages')); 
         }
+
+        if (isset($_GET['pagina']) && !empty($_GET['pagina']))
+        {
+            $page = intval($_GET['pagina']);
+
+            if ($page <= 0)
+            {
+                return redirect('site/produtos');
+            }
+        }
+
+        $items_per_page = 9;
+        $start_limit = $items_per_page * $page - $items_per_page;
+        $rows_count = App::get('database')->countAll('produtos');
         
-        $produtos = App::get('database')->selectAll('produtos');
+        if ($start_limit > $rows_count) {
+            return redirect('site/produtos');
+        }
+
+        $total_pages = ceil($rows_count / $items_per_page);
+
+        $produtos = App::get('database')->selectAll('produtos', $start_limit, $items_per_page);
         $categorias = App::get('database')->selectAll('categorias');
-        return view('site/produtos', compact('produtos', 'categorias'));
+        return view('site/produtos', compact('produtos', 'categorias', 'page', 'total_pages'));
 
     }
 
--- monstershop/app/views/site/produtos.view.php
+++ monstershop/app/views/site/produtos.view.php
@@ -91,22 +91,34 @@
     <!--fim cards-->
 
     <!--inicio paginação-->
-        
-      
     
     
-
-
-    <!--<div class="edit-paginacao">
-      <nav aria-label="Page navigation example ">
+  
+    <div class="edit-paginacao">
+      <nav aria-label="Page navigation" class="mt-3">
         <ul class="pagination">
-          <li class="page-item"><a class="page-link" href="#">Anterior</a></li>
-          <li class="page-item"><a class="page-link" href="#">1</a></li>
-          <li class="page-item"><a class="page-link" href="#">2</a></li>
-          <li class="page-item"><a class="page-link" href="#">Proximo</a></li>
+
+         <li class="page-item <?= $page <= 1 ? "disabled" : "" ?>">
+              <a class="page-link text-dark" href="?pagina=<?= $page > 1 ? $page - 1 : 1 ?>" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+              </a>
+          </li>
+
+          <?php for ($page_number = 1; $page_number <= $total_pages; $page_number++) : ?>
+              <li class="page-item <?= $page_number == $page ? "active" : "" ?>">
+                  <a class="page-link text-dark" href="?pagina=<?= $page_number ?>"><?= $page_number ?></a>
+              </li>
+          <?php endfor; ?>
+
+          <li class="page-item <?= $page >= $total_pages ? "disabled" : "" ?>">
+              <a class="page-link text-dark" href="?pagina=<?= $page < $total_pages ? $page + 1 : $total_pages ?>" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+              </a>
+          </li>
+
         </ul>
       </nav>
-    </div> -->
+    </div>
 
 
     <!--fim paginação-->

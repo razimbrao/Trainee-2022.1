dfeea60c8222de4d4b9c2b7fc15f3e2c7dea76cc
Fredericodgv <fredericodgv@gmail.com>
2022-06-09 20:26:08 -0300

    Cria rotas de Produtos

--- monstershop/app/routes.php
+++ monstershop/app/routes.php
@@ -50,6 +50,21 @@ $router->post('admin/usuarios/editar', 'UsuariosController@update');
 
 
 
+//---------Rotas de Produtos----------//
+
+
+$router->get('admin/produtos', 'ProdutosController@view');
+
+$router->post('admin/produtos/adicionar', 'ProdutosController@create');
+
+$router->post('admin/produtos/excluir', 'ProdutosController@delete');
+
+$router->post('admin/produtos/pesquisar', 'ProdutosController@show');
+
+$router->post('admin/produtos/editar', 'ProdutosController@update');
+
+
+
 //---------Rotas de Categorias----------//
 
 $router->get('admin/categorias', 'CategoriasController@index');

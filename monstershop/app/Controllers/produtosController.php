291823a598b4a76d03cf1bd887faad56c63f8ffa
razimbrao <rafaelozimbrao@gmail.com>
2022-06-15 11:41:26 -0300

    deletarImagens criado e QueryBuilder organizado

--- monstershop/app/Controllers/produtosController.php
+++ monstershop/app/Controllers/produtosController.php
@@ -12,14 +12,14 @@ class ProdutosController
     {
         if(!empty($_POST['pesquisa'])) {
             $produto = filter_input(INPUT_POST, 'pesquisa', FILTER_SANITIZE_SPECIAL_CHARS);  
-            $produtos = App::get('database')->search('produtos', $produto);
+            $produtos = App::get('database')->procurar('produtos', $produto);
             return view('admin/produtos', compact('produtos'));  
         }  
 
         $produtos = App::get('database')->selectAll('produtos');
 
         for ($i = 0; $i < count($produtos) ; $i++) { 
-            $produtoImagem = App::get('database')->selectImagem($produtos[$i]->id);
+            $produtoImagem = App::get('database')->selecionarNomeImagem($produtos[$i]->id);
             $produtos[$i]->imagens = $produtoImagem;
         }
 
@@ -34,11 +34,6 @@ class ProdutosController
         return view('admin/produtos', $table);
     }
 
-    public function show()
-    {
-        
-    }
-
     public function create()
     {
 
@@ -49,9 +44,9 @@ class ProdutosController
             'preco' => $_POST['preco'],
         ];
         
-        App::get('database')->insert('produtos', $parametros);
+        App::get('database')->adicionar('produtos', $parametros);
 
-        $produto_id = App::get('database')->selectProduto();
+        $produto_id = App::get('database')->selecionarIDProduto();
 
         $coluna = $_FILES['txtimagem']['name'];
 
@@ -61,19 +56,9 @@ class ProdutosController
                 'produtoID' => $produto_id[0]->id,
                 'nome_imagem' => $coluna[$i],
             ];
-            App::get('database')->insert('imagens', $imagens);
+            App::get('database')->adicionar('imagens', $imagens);
         }
         header('Location: /admin/produtos');
-    }
-
-    public function store()
-    {
-
-    }
-
-    public function edit()
-    {
-
     }
 
     public function update()
@@ -87,24 +72,24 @@ class ProdutosController
         
         $id = $_POST['id'];
         
-        App::get('database')->editaProdutos($id, 'produtos', $parametros);
+        App::get('database')->editar('produtos', $parametros, $id);
         
         $contador = false;
         $coluna = $_FILES['txtimagem']['name'];
-        if($coluna[0] != ""){
+        if($_FILES['txtimagem'] && $coluna[0] != ""){
             $contador = true;
         }
 
         if($contador){
-            App::get('database')->delete('imagens', $_POST['id']);
+            app::get('database')->deletarImagens($_POST['id']);
 
             for($i = 0; $i < sizeof($coluna); $i++){
                 $imagens = [
-                    'produtoID' => $_POST['id'],
+                    'produtoID' => $id,
                     'nome_imagem' => $coluna[$i],
                 ];
+                App::get('database')->adicionar('imagens', $imagens);
             }
-            App::get('database')->insert('imagens', $imagens);
         }
         
         header('Location: /admin/produtos');
@@ -113,8 +98,9 @@ class ProdutosController
 
     public function delete()
     {
+        app::get('database')->deletar('produtos', $_POST['id']);
+        app::get('database')->deletarImagens($_POST['id']);
 
-        app::get('database')->delete('produtos', $_POST['id']);
         header('Location: /admin/produtos');
     }
 }
--- monstershop/core/database/QueryBuilder.php
+++ monstershop/core/database/QueryBuilder.php
@@ -14,80 +14,112 @@ class QueryBuilder
         $this->pdo = $pdo;
     }
 
-    public function selectAll($table)
+    public function selectAll($table, $start_limit = null, $rows_amount = null)
     {
-        $sql = "select * from {$table}";
+        $sql = "select * from {$table}"; 
+
+        if  ($start_limit >= 0 && $rows_amount > 0)
+        {
+            $sql .= " LIMIT {$start_limit}, {$rows_amount}";
+        }
 
         try {
             $stmt = $this->pdo->prepare($sql);
 
             $stmt->execute();
 
-            return $stmt->fetchAll(PDO::FETCH_CLASS);
-        } catch (Exception $e) {
-            die($e->getMessage());
-        }
-    }
-    
-    public function delete($tabela, $id)
-    {
-        $sql = "delete from {$tabela} where id={$id}";
-
-
-        try {
-            $stmt = $this->pdo->prepare($sql);
-
-            $stmt->execute();
-
-            return $stmt->fetchAll(PDO::FETCH_CLASS);
+            return $stmt->fetchAll(PDO::FETCH_OBJ); 
         } catch (Exception $e) {
             die($e->getMessage());
         }
     }
 
-    public function editaProdutos($idp, $table, $parametros)
-    {
-        $sql = sprintf(
-            'UPDATE %s
-            SET %s
-            WHERE %s;', 
-            $table,
-            implode(', ', array_map(function ($parametros) {
-                return "{$parametros} = :{$parametros}";
-            }, array_keys($parametros))),
-            'id = :id'
-        );
+    //Funções Genéricas
 
-        $parametros['id'] = $idp;
 
-        try {
-            $statement = $this->pdo->prepare($sql);
-
-            $statement->execute($parametros);
-        } catch (Exception $e) {
-            die("An error occurred when trying to update database: {$e->getMessage()}");
-        }
-    }
-
-    
-    public function insert($table, $parametros)
+    public function adicionar($table, $dados)
     {
         $sql = sprintf(
             'INSERT INTO %s (%s) VALUES (%s)', 
-            $table, implode(',', array_keys($parametros)), 
-            ':' . implode(', :', array_keys($parametros))
+            $table, implode(',', array_keys($dados)), 
+            ':' . implode(', :', array_keys($dados))
         );
 
         try {
             $stmt = $this->pdo->prepare($sql);
 
-            $stmt->execute($parametros);
+            $stmt->execute($dados);
         } catch(Exception $e) {
             die($e->getMessage());
         }
     }
-   
-    public function selectImagem($id){
+
+    public function editar($table, $dados, $id)
+    {
+        $sql = sprintf(
+            'UPDATE %s SET %s WHERE %s', 
+            $table, implode(', ', array_map(function ($dados){
+                return "{$dados} = :{$dados}";
+            },
+            array_keys($dados))),
+            'id = :id'
+        );
+
+        $dados['id'] = $id;
+
+        try {
+            $stmt = $this->pdo->prepare($sql);
+
+            $stmt->execute($dados);
+        } catch(Exception $e) {
+            die($e->getMessage());
+        }
+    }
+
+
+    public function deletar($table, $id, $campoPesquisado = 'id')
+    {
+        
+        $sql = sprintf( 
+            'DELETE FROM %s WHERE %s;',
+            $table,
+            "$campoPesquisado = :id"
+        );
+
+        try {
+            $stmt = $this->pdo->prepare($sql);
+
+            $stmt->execute(compact('id'));
+        } catch(Exception $e) {
+            die($e->getMessage());
+        }
+    }
+
+
+    public function procurar($table, $nomeCampo, $campoPesquisado)
+    {
+
+        $sql = sprintf( 
+            'SELECT * FROM %s WHERE %s;',
+            $table,
+            "$nomeCampo like '%' :campoPesquisado '%' "
+        );
+
+        try {
+            $stmt = $this->pdo->prepare($sql);
+
+            $stmt->execute(compact('campoPesquisado'));
+            return $stmt->fetchAll(PDO::FETCH_OBJ);
+
+        } catch(Exception $e) {
+            die($e->getMessage());
+        }  
+    }
+
+
+    //Funcoes de Produtos
+
+    public function selecionarNomeImagem($id){
 
         $sql = 'SELECT nome_imagem FROM imagens WHERE imagens.produtoID = :id';
         
@@ -95,14 +127,14 @@ class QueryBuilder
 
             $stmt = $this->pdo->prepare($sql);
             $stmt->execute(["id"=> $id]);
-
+            
         } catch(Exception $e) {
             die($e->getMessage());
         }
     }
 
-    public function selectProduto(){
-
+    public function selecionarIDProduto(){
+        
         $sql = 'SELECT id FROM produtos ORDER BY id DESC LIMIT 1';
         
         try {
@@ -113,22 +145,19 @@ class QueryBuilder
             die($e->getMessage());
         }
     }
-
-    public function search($tabela, $nome){
-        $sql = sprintf( 
-            'SELECT * FROM %s WHERE %s;',
-            $tabela,
-            "nome like '%' :nome '%' "
-        );
-
+    
+    public function deletarImagens( $id)
+    {
+        $sql = "delete from 'imagens' where produtoID={$id}";
+        
         try {
             $stmt = $this->pdo->prepare($sql);
 
-            $stmt->execute(compact('nome'));
-            return $stmt->fetchAll(PDO::FETCH_OBJ);
+            $stmt->execute();
 
-        } catch(Exception $e) {
+            return $stmt->fetchAll(PDO::FETCH_CLASS);
+        } catch (Exception $e) {
             die($e->getMessage());
-        }  
+        }
     }
 }
